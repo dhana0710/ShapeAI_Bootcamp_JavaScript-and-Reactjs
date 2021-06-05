17263d3b25541fbcb5f78239e86aebec39cf6812
dhana0710 <83674577+dhana0710@users.noreply.github.com>
2021-06-06 01:50:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis veritatis perferendis totam facere. Quas quibusdam doloribus, praesentium accusamus ea sit iusto enim fuga explicabo quisquam quia!
+      </p>
+    </div>
+  );
+
+}
+
+export default Note;
